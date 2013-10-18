b99c80aa43c82c2a8685ac38b9303587c6ae9431
Will Bell <willbell@gmail.com>
2013-10-18 12:20:27 +0100

    Create initialize method for Plane and Airport class

---
+++ lib/airport.rb
@@ -0,0 +1,12 @@
+#something
+
+class Airport
+
+	attr_reader :capacity
+
+	def initialize(capacity=3)
+		@capacity = capacity
+	end
+
+
+end
---
+++ lib/plane.rb
@@ -0,0 +1,18 @@
+class Plane
+
+	attr_reader :status
+
+	def initialize 
+		@status = "flying"
+	end
+
+	# def request_to_land_at airport
+
+	# def land
+	# 	@status = "landed"
+	# end
+
+	#def take_off
+
+
+end
