17fece82ec594a34ccf1139633ac416e1e84dffc
Will Bell <willbell@gmail.com>
2013-10-18 21:28:38 +0100

    Finished functionality of project

--- spec/grand_finale_spec.rb
+++ spec/grand_finale_spec.rb
@@ -15,35 +15,25 @@ describe "The grand finale (last spec)" do
 	let (:plane4) { Plane.new }
 	let (:plane5) { Plane.new }
 	let (:plane6) { Plane.new }
+	let (:plane7) { Plane.new }
 
 	let (:airport) { Airport.new(6) }
 
 
 		it 'all planes can land' do
 			airport.stub(:weather_good? => true)
-			airport.land plane1
-			airport.land plane2
-			airport.land plane3
-			airport.land plane4
-			airport.land plane5
-			airport.land plane6
+			plane1.land_at airport
+			plane2.land_at airport
+			plane3.land_at airport
+			plane4.land_at airport
+			plane5.land_at airport
+			plane6.land_at airport
 			expect(airport.planes_on_ground).to eq [plane1, plane2, plane3, plane4, plane5, plane6]
 			expect(plane1.status).to eq "landed"
 			expect(plane5.status).to eq "landed"
-			plane7 = Plane.new
 			plane7.land_at airport
 			expect(airport.planes_on_ground).to eq [plane1, plane2, plane3, plane4, plane5, plane6]
 			expect(plane7.status).to eq "flying"
-		#end
-
-		# it 'plane cannot land if airport full' do
-		# 	plane7 = Plane.new
-		# 	plane7.land_at airport
-		# 	expect(airport.planes_on_ground).to eq [plane1, plane2, plane3, plane4, plane5, plane6]
-		# 	expect(plane7.status).to eq "flying"
-		# end
-
-		#it 'all planes can take off again' do
 			airport.request_take_off_to plane1
 			airport.request_take_off_to plane2
 			airport.request_take_off_to plane3
@@ -55,11 +45,25 @@ describe "The grand finale (last spec)" do
 			expect(plane2.status).to eq "flying"
 		end
 
-		it 'can land at airport of choice' do
+		it 'no planes land or take off if weather bad' do
+			airport.stub(:weather_good? => false)
+			plane1.land_at airport
+			plane2.land_at airport
+			plane3.land_at airport
+			plane4.land_at airport
+			plane5.land_at airport
+			plane6.land_at airport
+			expect(plane1.status).to eq "flying"
+			expect(plane4.status).to eq "flying"
+			expect(airport.planes_on_ground).to eq []
 			airport.stub(:weather_good? => true)
 			plane1.land_at airport
-			expect(plane1.status).to eq "landed"
-			expect(airport.planes_on_ground).to eq [plane1]
+			plane2.land_at airport
+			expect(airport.planes_on_ground).to eq [plane1, plane2]
+			airport.stub(:weather_good? => false)
+			airport.request_take_off_to plane1
+			airport.request_take_off_to plane2
+			expect(airport.planes_on_ground).to eq [plane1, plane2]
 		end
 
 end
